fe452d038e943c5f869e5cf80a8e6a0590f667fa
LAPTOP-G670LRM1\Eileen G <keighleen@yahoo.com>
2024-09-26 06:36:58 +0800

    cchecker, test

--- BeeniHackathon/BeeniHackathon/src/api/candidateChecker.ts
+++ BeeniHackathon/BeeniHackathon/src/api/candidateChecker.ts
@@ -5,13 +5,54 @@ const api = mande(`${import.meta.env.BASE_URL}api`)
 
 
 export const getDuplicates = async (candidate: CandidateRequest) => {
+    const a = {
+        /*
+        "firstName": "Scott",
+        "lastName": "Sheppard",
+        "birthDate": "8/31/1992",
+        "email": "perezjanet@example.org",
+        "phoneNumber": "421-429-7655x39421",
+        "address": "597 Smith Point",
+        "jobTitle": "Chief Tecology Officer",
+        "skills": "python, sql, power bi, .NET, C#, azure",
+        "gender": "Male",
+        "educationLevel": "High School",
+        "yearsOfExperience": "8",
+        "supplierName": "Amasarte",
+        "customerName": "EG Corp."
+        */
+        "firstName": "amirasadsdasd",
+        "lastName": "Sheppard",
+        "birthDate": "8/31/1992",
+        "email": "perezjanet@example.org",
+        "phoneNumber": "421-429-7655x39421",
+        "address": "597 Smith Point",
+        "jobTitle": "Chief Tecology Officer",
+        "skills": "python, pandas",
+        "gender": "",
+        "educationLevel": "",
+        "yearsOfExperience": "",
+        "supplierName": "",
+        "customerName": ""
+        
+    }
+
+
     const list = await api.post('/candidate/check', candidate).then((list: any) => {
         return <Array<CandidateResponse>>list
     })
+        /*, {
+        responseAs: 'response'
+    }).then(async (response: Response) => {
+        console.log(response.json);
+        //return await response.json
+        return response.json().then(response => ({ response }));
+    })*/
         .catch((error: any) => {
             console.log(error);
             throw error;
         })
+    console.log(list);
     return list;
 }
 
