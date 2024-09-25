a8f5d6b6d79e9a4afd92c4b45bdb3b7274363c13
LAPTOP-G670LRM1\Eileen G <keighleen@yahoo.com>
2024-09-26 06:36:17 +0800

    dirty & incomplete code

--- BeeniHackathon/BeeniHackathon/src/App.vue
+++ BeeniHackathon/BeeniHackathon/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <h1 class="green">Hello! </h1>
-    <RouterLink to="/create" id="candidateCreate">Create Candaidate</RouterLink>
+    <RouterLink to="/create" id="candidateCreate">Create Candidate</RouterLink>
     <RouterLink to="/list" id="candidateList">View Candidate List</RouterLink>
   </nav>
   <main>
